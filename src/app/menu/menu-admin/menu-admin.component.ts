99c1f86b88fc60f22dbed1ed055d20f4c1f85760
Vinicius Scampini <viniciuscscampini@gmail.com>
2023-11-17 16:55:18 -0300

    Criação da função de alterar a permissão do usuario

--- src/app/menu/menu-admin/menu-admin.component.ts
+++ src/app/menu/menu-admin/menu-admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuAcessoService } from '../service/menu-acesso.service';
 import { Operacao } from 'src/app/shared/operacao';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-menu-admin',
@@ -11,8 +11,11 @@ import { MessageService } from 'primeng/api';
 })
 export class MenuAdminComponent implements OnInit {
 
-  dados:any[] = []
-  dadoSelecionado:any[] = []
+  dados:any[] = [];
+  dadoSelecionado:any[] = [];
+
+  dadosUser:any[] = [];
+  dadoSelecionadoUser:any[] = [];
 
   valorFiltro:any = '';
 
@@ -25,11 +28,16 @@ export class MenuAdminComponent implements OnInit {
   acessarDadosTelaManter:boolean = false;
 
   desabilitarCamposManter:boolean = false;
+
+  indPodeExibirTabelaUser:boolean = false;
+
+  dadosUserPermissao:any = {};
   
   constructor(
     private router: Router,
     private menuAcessoService : MenuAcessoService,
     private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) { }
 
   ngOnInit(): void {
@@ -39,6 +47,10 @@ export class MenuAdminComponent implements OnInit {
   acessarDados(){
     this.menuAcessoService.buscarTodosDados().subscribe(dados => {
       this.dados = dados;
+		});
+
+    this.menuAcessoService.verificarUsername().subscribe(dados => {
+      this.dadosUser = dados;
 		});
   }
 
@@ -59,6 +71,40 @@ export class MenuAdminComponent implements OnInit {
       this.messageService.add({severity:'success', summary: 'Sucesso', detail: 'Excluido.', life: 3000});
       this.acessarDados();
     },
+      error => {
+        console.error('Erro ao adicionar dados:', error);
+      }
+    );
+  }
+
+  listarIndUser(event:any){
+    this.indPodeExibirTabelaUser = event;
+  }
+
+  atribuirPermissao(dados:any) {
+    this.confirmationService.confirm({
+        message: 'Você deseja tornar o acesso desse usuário como administrador?',
+        accept: () => {
+          this.salvarAlteracaoPermissao(dados, true);
+        }
+    });
+  }
+
+  retirarPermissao(dados:any) {
+    this.confirmationService.confirm({
+        message: 'Você deseja retirar o acesso administrador desse usuário??',
+        accept: () => {
+          this.salvarAlteracaoPermissao(dados, false);
+        }
+    });
+  }
+
+  salvarAlteracaoPermissao(dados:any, permissao:any){
+    dados.adminUser = permissao;
+    this.menuAcessoService.alterarUsername(dados).subscribe(
+      response => {
+        dados = response;
+      },
       error => {
         console.error('Erro ao adicionar dados:', error);
       }
